4127ad8f2a16f98376a9c697c92b3b20407fd72a
Penguin_Spy <me@penguinspy.dev>
2024-05-28 03:02:18 -0700

    add license

--- lib/connection.ex
+++ lib/connection.ex
@@ -1,3 +1,10 @@
+#  connection.ex © Penguin_Spy 2024
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# This Source Code Form is "Incompatible With Secondary Licenses", as
+# defined by the Mozilla Public License, v. 2.0.
+
 defmodule MC.Connection do
   require Logger
   import Bitwise
